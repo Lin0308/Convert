3941a2f45096522305b408e4b9c769ceac3674f9
Lin0308 <leo030889@gmail.com>
2023-08-25 15:19:34 +0800

    Third commit

--- app/src/main/java/com/example/read/MainActivity.java
+++ app/src/main/java/com/example/read/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -24,6 +25,19 @@ import android.widget.Toast;
 import android.widget.VideoView;
 import android.widget.MediaController;
 import android.widget.ImageView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
 import java.io.File;
 import java.io.IOException;
 import java.sql.Time;
@@ -40,19 +54,24 @@ public class MainActivity extends AppCompatActivity {
     private TextView txt;
     private Button Playbut,Convr;
     private static final String TAG = "hihihi";
+    LineChartData lineChartData;
+    LineChart lineChart;
+    ArrayList<String> xData = new ArrayList<>();
+    ArrayList<Entry> yData = new ArrayList<>();
+    Integer[][] PICCONV;
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            video_view =(VideoView) findViewById(R.id.video_view2);
-            img = (ImageView) findViewById(R.id.imageView);
-            Frame = (EditText) findViewById(R.id.editTextNumber);
-            txt = (TextView)findViewById(R.id.textView);
-            Playbut = (Button)findViewById(R.id.button);
-            Convr = (Button)findViewById(R.id.Convert);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        video_view =(VideoView) findViewById(R.id.video_view2);
+        img = (ImageView) findViewById(R.id.imageView);
+        Frame = (EditText) findViewById(R.id.editTextNumber);
+        txt = (TextView)findViewById(R.id.textView);
+        Playbut = (Button)findViewById(R.id.button);
+        Convr = (Button)findViewById(R.id.Convert);
+        lineChart = findViewById(R.id.Linechart);
 
-        }
-
+    }
         @Override
         protected void onActivityResult (int requestCode, int resaultCode, Intent data){
             super.onActivityResult(requestCode,resaultCode,data);
@@ -61,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 ArrayList<ArrayList<Integer>>Pix= new ArrayList<>();
 
 
+                lineChartData = new LineChartData(lineChart,this);
                 video_view.setVideoURI(uri1);
                 video_view.setKeepScreenOn(true);
                 MediaController controller = new MediaController(this);
@@ -70,48 +90,62 @@ public class MainActivity extends AppCompatActivity {
                 Playbut.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Long Time = Long.parseLong(Frame.getText().toString());
-                        Time = Time*1000000;
-
-                        processAndShowGrayscaleVideo(uri1, Time);
-                        //Log.d(TAG, "onClick: Pix " + Pix.get(1));
-                        //Toast.makeText(MainActivity.this,Pix.get(6),Toast.LENGTH_SHORT).show();
-                        //Toast.makeText(MainActivity.this,"playok", Toast.LENGTH_SHORT).show();
+                        try {
+                            Long Time = Long.parseLong(Frame.getText().toString());
+                            processAndShowGrayscaleVideo(uri1, Time);
+                        }catch (Exception e){
+                            Log.e(TAG, "Doesn't import Frame "+ e.toString() );
+                        }
                     }
                 });
                 Convr.setOnClickListener(new View.OnClickListener() {
-
                     public void onClick(View v) {
-
                         try {
                                 long VDur = video_view.getDuration();
-                                VDur = VDur/100;
-                                //Log.d(TAG, "Convert: Video Duration " + VDur);
-
-                                for (int i = 0 ; i <= 1 ;i++){
-                                    Bitmap gryPic = processAndShowGrayscaleVideo(uri1,(long)i*1000000);
+                                VDur = VDur/1000;
+                                Log.d(TAG, "Convert: Video Duration " + VDur);
+                                ArrayList rows = new ArrayList();
+                                Pix.clear();
+                                Integer[][] PICCONV = new Integer[(int)VDur][1000000];
+                                for (int i = 0 ; i < VDur ;i++){
+                                    Bitmap gryPic = processAndShowGrayscaleVideo(uri1,(long)i);
                                     int height =gryPic.getHeight();
                                     int width = gryPic.getWidth();
-                                    ArrayList rows = new ArrayList();
-                                    Pix.add(rows);
+                                    //Pix.add(rows);
                                     //Log.d(TAG, "width height " +width + height);
-                                    for (int j = width/4; j < width*3/4; j++) {
-                                        for (int k = height/3; k < height*2/3; k++){
-                                            Pix.get(0).add(gryPic.getPixel(j,k));
+                                    int z=0;
+                                    for (int j = width/3; j < width*2/3; j++) {
+                                        for (int k = height/4; k < height*3/4; k++){
+
+                                                PICCONV[i][z]=gryPic.getPixel(j, k);
+                                                z++;
+                                                //Pix.get(i).add(gryPic.getPixel(j,k));
+
                                         }
                                     }
                                     Log.d(TAG, "done pic "+i+" ph");
+                                    //Log.e(TAG, "PICCON.length : " + PICCONV.length);
+                                    //Log.e(TAG, "PICCON.1: " + PICCONV[i][1]);
+
                                 }
 
-
-
-
-
+                                    for (int l = 0; l < PICCONV.length; l++) {
+                                        //Log.d(TAG, "onClick: " + Pix.get(l).get(50));
+                                        Integer DataX = PICCONV[l][0];
+                                        Log.d(TAG, "PICCON.1: " + PICCONV[l][0]);
+                                        //Log.d(TAG, "pix.2: " + Pix.get(6));
+                                        yData.add(new Entry(DataX, l));
+                                        String num = String.valueOf(l);
+                                        xData.add(num);
+                                    }
+                                        lineChartData.initX(xData);
+                                        lineChartData.initY(-20000000F, 0F);
+                                        lineChartData.initDataSet(yData);
+                                        Log.d(TAG,"Ydata length " + yData);
+                                }catch (Exception e){
+                                    Log.e(TAG,"All error " + e.toString());
+                                }
                                 //Log.d(TAG, "Array all " + Pix.get(0).get(23) + Pix.get(0));
-                            }catch (Exception e){
-                                Log.e(TAG, "Convert But: Fail" + e.toString());
-                            }
-
                     }
                 });
             }
@@ -133,40 +167,39 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             intent.putExtra(MediaStore.EXTRA_OUTPUT, uri1);
             startActivityForResult(intent, 1);
-
-
         }
-
-
-        //把影片其中一個PIXEL取出 做方波顯示
         private Bitmap processAndShowGrayscaleVideo(Uri ur1,Long Time) {
-
 
                 MediaMetadataRetriever retriever = new MediaMetadataRetriever();
                 retriever.setDataSource(this, ur1);
 
                 String durationString = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
                 long duration = Long.parseLong(durationString);
-                //Log.d(TAG, "Convert: Video Duration " +duration);
+                duration = duration/1000;
+                //duration = duration*100;
+                Log.d(TAG, "processAndShowGrayscaleVideo: "+duration + " Time " + Time);
+                //Log.d(TAG, "Bitmap second: " + Time);
+
+                if (Time<duration) {
+                Time= Time *1000000;
                 Bitmap pic = retriever.getFrameAtTime(Time,MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
-                Bitmap Gpic =convertToGrayScale(pic);
+                Bitmap Gpic = convertToGrayScale(pic);
                 img.setImageBitmap(Gpic);
-
-               // Log.d(TAG,"pix: " +Pix.get(16));
-
-
-            return Gpic;
+                return Gpic;
+                  }
+                else {
+                    Log.e(TAG, "Fail frame at time "+Time );
+                    return null;
+            }
         }
-
         public Bitmap convertToGrayScale(Bitmap originalBitmap) {
                 int width = originalBitmap.getWidth();
                 int height = originalBitmap.getHeight();
                 //Log.d(TAG,"Orignal" +"width: "+ width +"height: "+height);
                 //Pix.add(originalBitmap.getPixel(width/2,height/2));
                 Bitmap grayScaleBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-
-                for (int i = width/4; i < width*3/4; i++) {
-                    for (int j = height/3; j < height*2/3; j++) {
+                for (int i = width/3; i < width*2/3; i++) {
+                    for (int j = height/4; j < height*3/4; j++){
                         int pixel = originalBitmap.getPixel(i, j);
                         int grayScale = (int) (Color.red(pixel) * 0.299 + Color.green(pixel) * 0.587 + Color.blue(pixel) * 0.114);
                         grayScaleBitmap.setPixel(i, j, Color.rgb(grayScale, grayScale, grayScale));
@@ -174,32 +207,20 @@ public class MainActivity extends AppCompatActivity {
                 }
                     return grayScaleBitmap;
     }
-
-       //public void DrawSqu(ArrayList){
-
-
-
-
-        //}
-
-
         private Uri getOutputFile(){
 
             File medidaStorageDir = new File(
                     Environment.getExternalStoragePublicDirectory(
                             Environment.DIRECTORY_PICTURES),"AnkaranaWork"
                     );
-
             if (!medidaStorageDir.exists()){
                 if(!medidaStorageDir.mkdir()){
                     Log.d("AnkaranaWork","failed to create directory");
                     return null;
                 }
             }
-
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
             File mediaFile = new File(medidaStorageDir.getPath() + File.separator + "VID_" + timeStamp + ".mp4");
-
             return Uri.fromFile(mediaFile);
         }
     }
